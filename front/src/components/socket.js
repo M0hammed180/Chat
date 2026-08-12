465dab6a02aa79a760b87359721669bc7129c14f
M0hammed180 <mohammed2006tare@gmail.com>
2026-08-12 04:39:33 +0300

    cc

--- front/src/components/socket.js
+++ front/src/components/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://chat-production-7ac6.up.railway.app", {
+export const socket = io("https://chat-production-7ac6.up.railway.app", {
   withCredentials: true,
   transports: ["websocket", "polling"], // إجبار استخدام الـ websocket أولاً
 });
